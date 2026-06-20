2c8c5297aece960d792b48e568e7eea75a51c67f
catorceno <camilacatorceno005@gmail.com>
2026-06-20 14:01:43 -0400

    test: HU-09 agregar test para mostrar alias o no en la tarjeta del calendario

--- src/scripts.tests/event.tests.js
+++ src/scripts.tests/event.tests.js
@@ -1,1 +1,30 @@
-describe('Event', () => {})
+import assert from "node:assert/strict";
+import { getEventCardLabel } from "../scripts/event.js";
+
+it("getEventCardLabel_aliasExistente_Exitoso", () => {
+  // Arrange
+  const event = {
+    title: "Ines Orellana Enriquez",
+    alias: "Inecita"
+  };
+
+  // Act
+  const cardLabel = getEventCardLabel(event);
+
+  // Assert
+  assert.equal(cardLabel, "Inecita");
+});
+
+it("getEventCardLabel_aliasInexistente_Exitoso", () => {
+  // Arrange
+  const event = {
+    title: "Ines Orellana Enriquez",
+    alias: ""
+  };
+
+  // Act
+  const cardLabel = getEventCardLabel(event);
+
+  // Assert
+  assert.equal(cardLabel, "Ines Orellana Enriquez");
+});
